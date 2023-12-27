a0cf4fdca268f6094eed3c807e1bcf1966917475
ludozico <121405886+ludozico@users.noreply.github.com>
2023-12-27 13:09:06 -0300

    git commit -m 'arlong.py'

--- arlong.py
+++ arlong.py
@@ -6,18 +6,37 @@ import datetime
 import pandas as pd
 
 # Load the CSV data into a pandas DataFrame and convert 'Start_date' to datetime format
-df = pd.read_csv('top100_kdrama.csv', parse_dates=['Start_date'], date_parser=lambda x: pd.to_datetime(x, errors='coerce', format='%m/%d/%Y'))
+df = pd.read_csv('top100_kdrama.csv')
+df['Start_date'] = pd.to_datetime(df['Start_date'], errors='coerce')
 
 # Filter rows where 'Start_date' is more recent than 01/01/2018
 df2 = df[df['Start_date'] > datetime.datetime(2018, 1, 1)]
 
 # Get a statistical summary of the data
-print(df2.describe())
 
-print(df2.columns)
 
 names = df2['Main Role'].str.split(', ', expand=True).stack()
-name_counts = names.value_counts()
+namesidx = names.index.get_level_values(0)
+name_counts = names.value_counts(ascending=False)
 
-print(name_counts)
+# Get the top 4 names
+top_names = name_counts.index[:4]
 
+# Create a function to check if any of the top names is in the 'Main Role'
+def contains_top_name(role):
+    for name in top_names:
+        if name in role:
+            return True
+    return False
+
+# Apply the function to the 'Main Role' column
+mask = df2['Main Role'].apply(contains_top_name)
+
+# Create the new DataFrame
+howfamous = df2[mask][['Main Role','Title','Score', 'Episodes', 'Watchers']]
+for i in howfamous.index:
+    role_names = howfamous.loc[i, 'Main Role'].split(', ')
+    howfamous.loc[i, 'Main Role'] = ', '.join(name for name in role_names if name in top_names)
+    
+howfamous.set_index('Main Role', inplace=True)
+print(howfamous)

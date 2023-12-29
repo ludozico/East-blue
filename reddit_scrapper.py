5969c3c9393dde65dbdffa949da59d8eb687f969
ludozico <121405886+ludozico@users.noreply.github.com>
2023-12-29 14:12:18 -0300

    git commit -m "added donkrieg.png"

--- reddit_scrapper.py
+++ reddit_scrapper.py
@@ -41,15 +41,29 @@ with open(CSV_FILENAME, 'w', newline='', encoding='utf-8') as csvfile:
     for submission in top_posts:
         # Reddit doesn't load all comments by default, you must explicitly ask for more
         submission.comment_sort = 'top'  # sort by top comments
-        submission.comments.replace_more(limit=0)  # This line is necessary to get all top-level comments
-        top_comments = submission.comments.list()[:3]  # Get the top 3 comments
-        
+        submission.comments.replace_more(
+            limit=0
+        )  # This line is necessary to get all top-level comments
+
+        # Filter out comments that start with '**Mod Note**'
+        filtered_comments = [
+            comment
+            for comment in submission.comments.list()
+            if not comment.body.startswith('**Mod Note**')
+        ]
+
+        # Get the top 3 comments, excluding those that start with '**Mod Note**'
+        top_comments = filtered_comments[:3]
+
         for comment in top_comments:
             # Write the data to the CSV file with the available information (no upvote ratio for comments)
-            csvwriter.writerow([submission.title, submission.url, submission.score, comment.body, comment.score])
+            csvwriter.writerow([
+                submission.title,
+                submission.url,
+                submission.score,
+                comment.body,
+                comment.score,
+            ])
 
 # Let the user know the data has been saved
 print(f"Data saved to {CSV_FILENAME}\n")
-
-
-
---
+++ redditdatavisual.py
@@ -0,0 +1,23 @@
+''' This script is used to visualize the data from the redditdata.csv file. It creates a heatmap of the comment scores on the KDRAMA subreddit. '''
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+redditdata = pd.read_csv('C:/Users/ludoz/OneDrive/Documents/GitHub/East-blue/redditdata.csv')
+redditdata_subset1 = pd.DataFrame(redditdata)
+
+redditdata_subset1['post_title'] = redditdata_subset1['post_title'].str.wrap(30)
+redditdata_subset1['comment_body'] = redditdata_subset1['comment_body'].str.wrap(30)
+redditdata_subset1['post_title'] = redditdata_subset1['post_title'].str.slice(0, 100)
+redditdata_subset1['comment_body'] = redditdata_subset1['comment_body'].str.slice(0, 100)
+redditdata_subset1 = redditdata_subset1.pivot(index="comment_body", columns="post_title", values="comment_score")
+
+plt.figure(figsize=(30,30))
+
+plt.title('Heatmap of comment scores on KDRAMA subreddit')
+sns.set(style="darkgrid", font_scale=1.2)
+
+donkrieg = sns.heatmap(redditdata_subset1, annot=True, cmap="coolwarm", fmt='.1f', linewidths=.5, annot_kws={"size": 10})
+plt.savefig('C:/Users/ludoz/OneDrive/Documents/GitHub/East-blue/donkrieg.png',format='png', dpi=300, bbox_inches='tight')

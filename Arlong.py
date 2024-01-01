04aaafcf52a6d2e213352b596ddf0d0446b276fd
Ludozico <luizfillipeps@icloud.com>
2023-12-31 21:40:40 -0300

    git commit -m "feliz2024"

--- Arlong.py
+++ Arlong.py
@@ -1,4 +1,6 @@
-item1 = 'Phone'
-item1_price = 100
-item1_quantity = 5
-item1_price_total = item1_price * item1_quantity 
+""" Intermediary Python! Let's go!"""
+
+ITEM1 = 'Phone'
+ITEM1_PRICE = 100
+ITEM1_QUANTITY = 5
+ITEM1_PRICE_TOTAL = ITEM1_PRICE * ITEM1_QUANTITY

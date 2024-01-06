216564d2ef72dd97758610c975f33f38622c8658
Luiz Fillipe Pinto da Silva <121405886+ludozico@users.noreply.github.com>
2024-01-06 18:19:27 -0300

    Update Arlong.py

--- Arlong.py
+++ Arlong.py
@@ -1,13 +1,14 @@
+""" Trying someshit"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pylab as plt
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.stattools import acf, pacf
 from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima.model import ARIMA
 from matplotlib.pylab import rcParams
 rcParams['figure.figsize'] = 10, 6
-
 PATH = 'C:/Users/ludoz/Downloads/predict-energy-behavior-of-prosumers/'
 
 train=pd.read_csv(PATH+'train.csv')
@@ -17,11 +18,6 @@ electricity=pd.read_csv(PATH+'electricity_prices.csv')
 forecast_w=pd.read_csv(PATH+'forecast_weather.csv')
 hist_w=pd.read_csv(PATH+'historical_weather.csv')
 w_s_c=pd.read_csv(PATH+'weather_station_to_county_mapping.csv')
-
-# filling out missing values
-train['target'] = train['target'].interpolate(method='linear')
-# dummies for the categorical variables in train
-train_encoded = pd.get_dummies(train, columns=['county', 'product_type'])
 
 # filling out missing values
 train['target'] = train['target'].interpolate(method='linear')
@@ -45,10 +41,11 @@ hist_w = hist_w.set_index('datetime').sort_index()
 
 # Let's visualize the target variable over time quickly
 train['target'].plot(title='Target Variable Over Time')
-result = adfuller(train['target'][:1000], autolag='AIC')
-dfoutput = pd.Series(result[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+result = adfuller(train['target'][:100000], autolag='AIC')
+dfoutput = pd.Series(result[0:4], index=['Test Statistic','p-value',
+                                         '#Lags Used','Number of Observations Used'])
 for key,value in result[4].items():
-    dfoutput['Critical Value (%s)'%key] = value 
+    dfoutput['Critical Value (%s)'%key] = value
 print(dfoutput)
 
 print(f'ADF Statistic: {result[0]}')
@@ -59,7 +56,7 @@ else:
     print("The series is not stationary.")
 
 # Decompose the series to observe trend and seasonality
-decomposition = seasonal_decompose(train['target'][:1000].dropna(),
+decomposition = seasonal_decompose(train['target'][:100000].dropna(),
                                    model='additive', period=24)
 # assuming hourly data has a daily seasonality
 trend = decomposition.trend
@@ -87,23 +84,36 @@ pacf_result = pacf(train['target'], nlags=72)
 plt.figure(figsize=(15, 5))
 plt.subplot(121)
 plt.plot(acf_result)
-plt.title('Autocorrelation Function')
+plt.xticks(np.arange(0, 71, 1))
 
 plt.subplot(122)
 plt.plot(pacf_result)
+plt.xticks(np.arange(0, 71, 1))
 plt.title('Partial Autocorrelation Function')
 plt.tight_layout()
 plt.show()
 
 #Determine rolling statistics
-rolmean = train['target'][1000].rolling(window=3164).mean()
-rolstd = train['target'][1000].rolling(window=3164).std()
+rolmean = train['target'].rolling(window=201835).mean()
+rolstd = train['target'].rolling(window=201835).std()
 
 #Plot rolling statistics
 orig = plt.plot(train['target'], color='blue', label='Original')
 mean = plt.plot(rolmean, color='red', label='Rolling Mean')
-std = plt.plot(rolstd, color='black', label='Rolling  Std')
-plt.legend(loc='best')
-plt.title('Rolling Mean & Standard Deviation')
 plt.show(block=False)
 
+# AR model
+model = ARIMA(train['target'][:100000], order=(1,0,0))
+results_AR = model.fit()
+plt.plot(train['target'][:100000])
+plt.plot(results_AR.fittedvalues, color='red')
+plt.title(f'RSS: {sum((results_AR.fittedvalues - train["target"][:100000])**2):.4f}')
+print('Plotting AR model')
+
+# MA model
+model = ARIMA(train['target'][:100000], order=(0,0,1))
+results_MA = model.fit()
+plt.plot(train['target'][:100000])
+plt.plot(results_MA.fittedvalues, color='red')
+plt.title(f'RSS: {sum((results_MA.fittedvalues - train["target"][:100000])**2):.4f}')
+print('Plotting MA model')
